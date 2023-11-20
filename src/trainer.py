a1bc33adb3b2cc94c1596e9921dfd6b917202281
Hughie Phan <phanthanhhuy1996@gmail.com>
2023-11-20 17:05:17 +0700

    Update trainer.py

--- src/trainer.py
+++ src/trainer.py
@@ -14,7 +14,8 @@ if '../src' not in sys.path:
     sys.path.append('../src')
 
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 import time
 from src.data import BatchLoader
 
